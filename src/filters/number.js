75c38da5bd93a1d63ceb4d064ab2e227f427d83f
yangjian <i@yangjian.me>
2016-05-19 17:25:17 +0800

    build

--- src/filters/number.js
+++
@@ -1,8 +0,0 @@
-import Util from '../assets/util.js'
-
-export default {
-  // 金额统一格式化
-  numberFormat: function (value, decimals, decPoint, thousandsSep, decMethod) {
-    return Util.numberFormat(value, decimals || 2, decPoint, thousandsSep, decMethod)
-  }
-}
